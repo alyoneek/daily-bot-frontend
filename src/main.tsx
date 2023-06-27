bc5b4aff72f6c5ce46883e4e5f15646a4aa0f830
alyoneek <alyonta03@mail.ru>
2023-06-28 03:19:24 +0700

    (config) Add provider for theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import { router } from "@/router/routes.tsx";
 import { store } from "@/store";
+import { ConfigProvider, theme } from "antd";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import "./index.css";
 
+const { defaultAlgorithm, darkAlgorithm } = theme;
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <ConfigProvider
+      theme={{
+        algorithm: defaultAlgorithm,
+      }}>
+      <RouterProvider router={router} />
+    </ConfigProvider>
   </Provider>,
 );
